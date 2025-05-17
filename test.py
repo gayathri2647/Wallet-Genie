1c59e1929138a3df9f8e929bd03892db45754c8c
M.N.Gayathri Prasad <gayathriprasadns@gmail.com>
2025-05-17 21:08:30 +0530

    added requirement

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print("hello")
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
+key = get_random_bytes(16)
+cipher = AES.new(key, AES.MODE_EAX)
+
+print("Key:", key)
+print("Cipher:", cipher)
