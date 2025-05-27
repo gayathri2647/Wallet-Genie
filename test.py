9a26d76c4534d3173351714d4f095e57737ac980
Yugesh A <a.yugesh2@gmail.com>
2025-05-27 06:59:00 +0530

    todo list update

--- test.py
+++
@@ -1,8 +0,0 @@
-from Crypto.Cipher import AES
-from Crypto.Random import get_random_bytes
-
-key = get_random_bytes(16)
-cipher = AES.new(key, AES.MODE_EAX)
-
-print("Key:", key)
-print("Cipher:", cipher)
